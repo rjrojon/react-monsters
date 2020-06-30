afbfd9fda0aa15e16ce7d03a150f37dd00a4848f
rjrojon <rjrojon@gmail.com>
2020-07-01 01:58:38 +0600

    Updates

---
+++ precache-manifest.145714e981d71e227ff41c8e99705c6f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70a08369b2a314eda0a332d5b7b39345",
+    "url": "/react-monsters/index.html"
+  },
+  {
+    "revision": "1354e5e9c378c497dcdd",
+    "url": "/react-monsters/static/css/main.1bf93fb8.chunk.css"
+  },
+  {
+    "revision": "045c2ed955cec24ea193",
+    "url": "/react-monsters/static/js/2.6931aef1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-monsters/static/js/2.6931aef1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1354e5e9c378c497dcdd",
+    "url": "/react-monsters/static/js/main.aeaf0a64.chunk.js"
+  },
+  {
+    "revision": "10a25d6082fe69f22468",
+    "url": "/react-monsters/static/js/runtime-main.578e257e.js"
+  }
+]);
